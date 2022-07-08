652422ee6180c4b5c8b773600e5687c546483375
chan <cham9994@gmail.com>
2022-07-08 21:44:30 +0900

    🎉  install firebase

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDXWduk6-QV8HwPHiz9eANMhIArIZIJ0R4',
+  authDomain: 'cwitter-88dbf.firebaseapp.com',
+  projectId: 'cwitter-88dbf',
+  storageBucket: 'cwitter-88dbf.appspot.com',
+  messagingSenderId: '1022652853109',
+  appId: '1:1022652853109:web:c51179a9813111c9c1413c'
+}
+
+export const app = initializeApp(firebaseConfig)
