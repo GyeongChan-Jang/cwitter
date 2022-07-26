e1074d6fd071b0c77776dfbda0f25a5955098870
chan <cham9994@gmail.com>
2022-07-26 17:51:19 +0900

    ✨  firebase를 통한 로그인/회원가입 구현

--- src/components/Router.js
+++ src/components/Router.js
@@ -3,14 +3,11 @@ import { Routes, Route, Outlet, Link } from 'react-router-dom'
 import Auth from '../routes/Auth'
 import Home from '../routes/Home'
 
-const AppRouter = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false)
+const AppRouter = ({ isLoggedIn }) => {
   return (
     <Routes>
       {isLoggedIn ? (
-        <>
-          <Route path="/" element={<Home />} />
-        </>
+        <Route path="/" element={<Home />} />
       ) : (
         <Route path="/" element={<Auth />} />
       )}
